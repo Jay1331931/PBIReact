cfc401e827b8ac8b447b349691d9fa11eb3ce623
Jay-abs <jay.shah@abs.in>
2026-03-31 18:03:46 +0530

    Initial commit after url change

--- backend/cron/index.js
+++ backend/cron/index.js
@@ -1,8 +1,8 @@
 const { startSyncJob } = require("./syncReports.cron");
 
 function initCronJobs() {
-  console.log("🚀 Initializing cron jobs...");
-  startSyncJob();
+  // console.log("🚀 Initializing cron jobs...");
+  // startSyncJob();
 }
 
 module.exports = { initCronJobs };
